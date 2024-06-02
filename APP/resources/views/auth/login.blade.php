027590d8746cc79c2f65b9b1326b7b7ca08e01fe
M.Farhan Al Ihrami <90372325+1122Arhan@users.noreply.github.com>
2024-06-02 19:46:46 +0700

    register dan login

--- APP/resources/views/auth/login.blade.php
+++ APP/resources/views/auth/login.blade.php
@@ -35,7 +35,7 @@
                     <div class="card-body">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-3"></div>
+                            <div class="col-lg-3">
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
